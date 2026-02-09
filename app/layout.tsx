5768b8563474614a7598ffb14244d5d18d911568
Brock Rose <brock@macbook-air-2.lan>
2026-02-08 22:48:32 -0600

    googletag

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17941320563"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'AW-17941320563');`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
